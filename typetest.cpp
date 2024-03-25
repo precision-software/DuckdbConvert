490c9dc5dacdc6ba5256e8d46163b63593747801
John Morris <john.morris@crunchydata.com>
2024-03-25 10:23:10 -0700

    created .gitignore and cleaned up typetest

---
+++ typetest.cpp
@@ -0,0 +1,40 @@
+/*
+ *
+ */
+
+#include "duckdb.hpp"
+#include "duckdbconvert.hpp"
+
+using namespace duckdb;
+
+/*
+ * A quick test program to explore converting DuckDb types to Postgres types.
+ * This version scans the DuckDb logical type tree rather than parsing the type strings.
+ */
+int main()
+{
+	DuckDB db(nullptr);
+	Connection con(db);
+
+	/* An SQL statement with xxxx types */
+	const char *sql =
+		"SELECT {'birds':"
+		"			{'yes': 'duck', 'maybe': 'goose', 'huh': NULL, 'no': 'heron'},"
+		"		'aliens':"
+		"			NULL,"
+		"		'amphibians':"
+		"			{'yes':'frog', 'maybe': 'salamander', 'huh': 'dragon', 'no':'toad'},"
+		"        'row': (1, 107.66, 3.5, 17),"
+		"        'map': MAP(['abc', 'def', 'efg'], [1.23, 4.56, 7.99]::float[])"
+		"}";
+
+	auto result = con.Query(sql);
+
+	for (LogicalType type: result->types)
+		sendDuckType(type);
+
+	while(auto chunk = result->Fetch())
+		sendDuckData(chunk.get());
+
+	return 0;
+}
